6a0faac0a5d6bb35836a34ca2f949b2286409a47
Rep Graphics <69086745+RepGraphics@users.noreply.github.com>
2023-04-16 18:29:32 +0100

    Add files via upload

--- assets/js/XO-Lookup.js
+++ assets/js/XO-Lookup.js
@@ -1,19 +1,20 @@
+
+
 $(document).ready(function() {
   if (window.location.pathname.includes('/XBOX-Official.html')) {
     $('#search').on('click', function() {
-      let query = $('#search-bar').val().toLowerCase();
+      const query = $('#search-bar').val().toLowerCase();
       $.ajax({
         method: "GET",
         url: 'https://corsproxy.io/?http://arkdedicated.com/xbox/cache/officialserverlist.json',
         dataType: 'json',
-        success: function(data) {
+         success: function(data) {
           const filteredData = data.filter(function(server) {
             return server.Name.toLowerCase().includes(query);
           });
           const tbody = $('#table tbody');
           tbody.empty();
-          for (let i = 0; i < filteredData.length; i++) {
-            const server = filteredData[i];
+          filteredData.forEach(function(server) {
             const tr = $('<tr>').append(
               $('<td>').text(server.Name),
               $('<td>').text(server.MapName),
@@ -21,7 +22,7 @@ $(document).ready(function() {
               $('<td>').text(`${server.NumPlayers}/${server.MaxPlayers}`)
             );
             tbody.append(tr);
-          }
+          });
         },
         error: function() {
           const tbody = $('#table tbody');
@@ -34,42 +35,40 @@ $(document).ready(function() {
       });
     });
 
-    const searchInput = document.getElementById("search-bar");
-
-    searchInput.addEventListener("keydown", function(event) {
-      if (event.key === "Enter") {
-        let query = $('#search-bar').val().toLowerCase();
-        $.ajax({
-          method: "GET",
-          url: 'https://corsproxy.io/?http://arkdedicated.com/xbox/cache/officialserverlist.json',
-          dataType: 'json',
-          success: function(data) {
-            const filteredData = data.filter(function(server) {
-              return server.Name.toLowerCase().includes(query);
-            });
-            const tbody = $('#table tbody');
-            tbody.empty();
-            for (let i = 0; i < filteredData.length; i++) {
-              const server = filteredData[i];
-              const tr = $('<tr>').append(
-                $('<td>').text(server.Name),
-                $('<td>').text(server.MapName),
-                $('<td>').text(server.DayTime),
-                $('<td>').text(`${server.NumPlayers}/${server.MaxPlayers}`)
-              );
-              tbody.append(tr);
-            }
-          },
-          error: function() {
-            const tbody = $('#table tbody');
-            tbody.empty();
-            const tr = $('<tr>').append(
-              $('<td>').text('Failed to fetch data from the API')
-            );
-            tbody.append(tr);
-          }
+const searchInput = document.getElementById("search-bar");
+searchInput.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    const query = $('#search-bar').val().toLowerCase();
+    $.ajax({
+      method: "GET",
+      url: 'https://corsproxy.io/?http://arkdedicated.com/xbox/cache/officialserverlist.json',
+      dataType: 'json',
+       success: function(data) {
+        const filteredData = data.filter(function(server) {
+          return server.Name.toLowerCase().includes(query);
         });
+        const tbody = $('#table tbody');
+        tbody.empty();
+        filteredData.forEach(function(server) {
+          const tr = $('<tr>').append(
+            $('<td>').text(server.Name),
+            $('<td>').text(server.MapName),
+            $('<td>').text(server.DayTime),
+            $('<td>').text(`${server.NumPlayers}/${server.MaxPlayers}`)
+          );
+          tbody.append(tr);
+        });
+      },
+      error: function() {
+        const tbody = $('#table tbody');
+        tbody.empty();
+        const tr = $('<tr>').append(
+          $('<td>').text('Failed to fetch data from the API')
+        );
+        tbody.append(tr);
       }
     });
+  }
+});
   }
 });
